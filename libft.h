08ecd941976725737dc6370751fb61a8e69d0c39
Richard Kosa <165525466+Hattyuh@users.noreply.github.com>
2026-04-10 15:30:22 +0000

    add: ft_memchr.c
    implemented memchr

--- libft.h
+++ libft.h
@@ -23,5 +23,6 @@ char            *ft_strdup(const char *str);
 void            *ft_calloc(size_t number, size_t size);
 void            *ft_memset(void *s, int c, size_t n);
 void            *ft_memcpy(void *dest, const void *src, size_t n);
+void            *ft_memchr(const void *s, int c, size_t n);
 
 #endif
