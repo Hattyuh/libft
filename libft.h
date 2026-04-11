49dc4176827b04d4df0b9616da098430836510a0
rkosa <kosa.richardd@gmail.com>
2026-04-11 17:29:24 +0200

    add: ft_bzero
    calls memset with a zero

--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ void            *ft_calloc(size_t number, size_t size);
 void            *ft_memset(void *s, int c, size_t n);
 void            *ft_memcpy(void *dest, const void *src, size_t n);
 void            *ft_memchr(const void *s, int c, size_t n);
+void            ft_bzero(void *s, size_t n);
 
 #endif
