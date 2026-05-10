6501f80baabac5347c9f0c30be7660e461745585
Richard Kosa <rkosa@c2r1p8.42vienna.com>
2026-05-10 23:47:24 +0200

    fix: split
    it works now but still need to add protection in case malloc fails

--- ft_split.c
+++ ft_split.c
@@ -12,69 +12,104 @@
 
 #include "libft.h"
 
-static size_t   ft_countwords(const char *str, char c)
+static size_t	ft_countwords(const char *str, char c)
 {
-    int     i;
-    int     inword;
-    size_t  wordcount;
+	int		i;
+	int		inword;
+	size_t	wordcount;
 
-    wordcount = 0;
-    inword = 0;
-    i = 0;
-    while (str[i])
-    {
-        while (str[i] == c)
-            i++;
-        if (str[i])
-        {
-            wordcount++;
-            inword = 1;
-        }
-        while (inword && str[i])
-        {
-            if (str[i] == c)
-                inword = 0;
-            i++;
-        } 
-    }
-    return (wordcount);
+	wordcount = 0;
+	inword = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i])
+		{
+			wordcount++;
+			inword = 1;
+		}
+		while (inword && str[i])
+		{
+			if (str[i] == c)
+				inword = 0;
+			i++;
+		}
+	}
+	return (wordcount);
 }
 
-static char *ft_extractword(const char *wordstart, char c)
+static char	*ft_getword(const char *wordstart, char c)
 {
-    char    *word;
-    size_t  len;
+	char	*word;
+	size_t	len;
 
-    len = 0;
-    while (wordstart[len] != c && wordstart[len] != '\0')
-        len++;
-    word = malloc(len + 1);
-    if (!word)
-        return (NULL);
-    ft_memcpy(word, wordstart, len);
-    word[len] = '\0';
-    return (word);
+	len = 0;
+	while (wordstart[len] != c && wordstart[len] != '\0')
+		len++;
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	ft_memcpy(word, wordstart, len);
+	word[len] = '\0';
+	return (word);
+}
+
+char	**ft_split(const char *str, char c)
+{
+	int		i;
+	int		inword;
+	size_t	wordcount;
+	size_t	currentword;
+	char	**words;
+
+	currentword = 0;
+	wordcount = ft_countwords(str, c);
+	words = malloc((wordcount + 1) * sizeof(char *));
+	if (!words)
+	{
+		return (NULL);
+	}
+	inword = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i])
+		{
+			words[currentword] = ft_getword(&str[i], c);
+			currentword++;
+			inword = 1;
+		}
+		while (inword && str[i])
+		{
+			if (str[i] == c)
+				inword = 0;
+			i++;
+		}
+	}
+	words[currentword] = NULL;
+	return (words);
 }
 
 #include <stdio.h>
+
 int	main(void)
 {
-    char    *str = "Afew words that needs to be   separate";
-    char    c = ' ';
-    // char    **words;
-    // int     i;
-    //words = ft_split(str, c);
-    char    *extractedword;
+	char *str = "A few words that needs to be   separated";
+	char c = ' ';
+	char **words;
+	int i;
 
-    extractedword = ft_extractword(str, c);
-    printf("Word count:     %lu\n", ft_countwords(str, c));
-    printf("Extracted word: %s\n", extractedword);
-    // i = 0;
-    // while (i < 8)
-    // {
-    //     printf(words[i]);
-    //     i++;
-    // }
-    free(extractedword);
+	words = ft_split(str, c);
+	i = 0;
+	while (words[i])
+	{
+		printf("%s\n", words[i]);
+		i++;
+	}
+	free(words);
 	return (0);
 }
